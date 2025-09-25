13b85314e5fba8241fa00753492b2fa546bee52c
lulu08-G <lucas_91@live.fr>
2025-09-25 11:53:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
 
+st.title("🧗 Analyseur de voies d’escalade")
+
+uploaded_file = st.file_uploader("Upload une photo de la voie", type=["jpg", "png"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Voie uploadée")
+
+    st.write("🚧 Détection des prises à implémenter...")
